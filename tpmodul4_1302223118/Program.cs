8f808a51c84b62f3b5c377f50d69ad1768a44526
rakha <rakhagalih19@gmail.com>
2024-03-10 15:40:13 +0700

    menambahkan implementasi dengan state-based construction

--- tpmodul4_1302223118/Program.cs
+++ tpmodul4_1302223118/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Nomor 3 A-C : \n");
         KodePos kodePos = new KodePos();
         Console.WriteLine("Kode pos Batununggal\t: " + kodePos.GetKodePos("Batununggal"));
         Console.WriteLine("Kode pos Kujangsari\t: " + kodePos.GetKodePos("Kujangsari"));
@@ -18,5 +19,15 @@ internal class Program
         Console.WriteLine("Kode pos Samoja\t\t: " + kodePos.GetKodePos("Samoja"));
 
         // Commit "menambahkan implementasi dengan table-driven"
+
+        Console.WriteLine("\n\n\nNomor 5 A-E : \n");
+
+        DoorMachine door = new DoorMachine();
+        door.DoAction(Action.BukaPintu);
+        door.DoAction(Action.KunciPintu);
+        door.DoAction(Action.KunciPintu); // Kunci lagi, tidak akan terjadi apa-apa karena pintu sudah terkunci
+        door.DoAction(Action.BukaPintu);
+
+        // Commit "menambahkan implementasi dengan state-based construction"
     }
 }
