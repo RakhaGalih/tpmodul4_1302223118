08ebd01acb42272b8a96adf26481e5721b98bcbd
rakha <rakhagalih19@gmail.com>
2024-03-10 13:05:51 +0700

    "menambahkan implementasi dengan table-driven"

---
+++ tpmodul4_1302223118/KodePos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace tpmodul4_1302223118;
+
+using System.Collections.Generic;
+
+public class KodePos
+{
+    private Dictionary<string, string> kodePosDictionary;
+
+    public KodePos()
+    {
+        kodePosDictionary = new Dictionary<string, string>()
+        {
+            {"Batununggal", "40266"},
+            {"Kujangsari", "40287"},
+            {"Mengger", "40267"},
+            {"Wates", "40256"},
+            {"Cijaura", "40287"},
+            {"Jatisari", "40286"},
+            {"Margasari", "40286"},
+            {"Sekejati", "40286"},
+            {"Kebonwaru", "40272"},
+            {"Maleer", "40274"},
+            {"Samoja", "40273"}
+        };
+    }
+
+    public string GetKodePos(string kelurahan)
+    {
+        return kodePosDictionary.ContainsKey(kelurahan) ? kodePosDictionary[kelurahan] : "Kode Pos Tidak Ditemukan";
+    }
+}
+
+
---
+++ tpmodul4_1302223118/Program.cs
@@ -0,0 +1,22 @@
+﻿using tpmodul4_1302223118;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KodePos kodePos = new KodePos();
+        Console.WriteLine("Kode pos Batununggal\t: " + kodePos.GetKodePos("Batununggal"));
+        Console.WriteLine("Kode pos Kujangsari\t: " + kodePos.GetKodePos("Kujangsari"));
+        Console.WriteLine("Kode pos Mengger\t: " + kodePos.GetKodePos("Mengger"));
+        Console.WriteLine("Kode pos Wates\t\t: " + kodePos.GetKodePos("Wates"));
+        Console.WriteLine("Kode pos Cijaura\t: " + kodePos.GetKodePos("Cijaura"));
+        Console.WriteLine("Kode pos Jatisari\t: " + kodePos.GetKodePos("Jatisari"));
+        Console.WriteLine("Kode pos Margasari\t: " + kodePos.GetKodePos("Margasari"));
+        Console.WriteLine("Kode pos Sekejati\t: " + kodePos.GetKodePos("Sekejati"));
+        Console.WriteLine("Kode pos Kebonwaru\t: " + kodePos.GetKodePos("Kebonwaru"));
+        Console.WriteLine("Kode pos Maleer\t\t: " + kodePos.GetKodePos("Maleer"));
+        Console.WriteLine("Kode pos Samoja\t\t: " + kodePos.GetKodePos("Samoja"));
+
+        // Commit "menambahkan implementasi dengan table-driven"
+    }
+}
